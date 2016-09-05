0d28ee2ca982d1c7ba9b50e6ccaa2dc4156cc174
hanmaoChen <771536653@qq.com>
2016-09-05 21:33:07 +0800

    新特性界面创建UIScrollView

--- ZFBNewFeatureView.m
+++ ZFBNewFeatureView.m
@@ -10,4 +10,22 @@
 
 @implementation ZFBNewFeatureView
 
+
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    
+    if (self = [super initWithFrame:frame]) {
+        
+        [self setupUI];
+    }
+    return self;
+}
+#pragma mark - 初始化控件
+-(void)setupUI{
+//    创建UIScrollView
+    UIScrollView *sV = [[UIScrollView alloc]initWithFrame:self.bounds];
+    
+    [self addSubview:sV];
+    
+}
 @end
