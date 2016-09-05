19476d189069fbfcfff930e69664c501581fd10d
hanmaoChen <771536653@qq.com>
2016-09-05 20:54:06 +0800

    搭建主界面

--- ZFBMainViewController.m
+++ ZFBMainViewController.m
@@ -7,7 +7,37 @@
 //
 
 #import "ZFBMainViewController.h"
+#import "Masonry.h"
+@interface ZFBMainViewController ()
+
+@end
 
 @implementation ZFBMainViewController
 
+- (void)viewDidLoad{
+    [super viewDidLoad];
+//    初始化主界面
+    [self setupUI];
+}
+
+
+#pragma mark - 初始化主界面
+-(void)setupUI{
+    
+    UIImageView *imgV = [[UIImageView alloc]init];
+    
+    imgV.image = [UIImage imageNamed:@"cozy-control-car"];
+    
+    [self.view addSubview:imgV];
+    
+//    调整大小
+    imgV.contentMode = UIViewContentModeScaleAspectFit;
+
+//    添加约束
+[imgV mas_makeConstraints:^(MASConstraintMaker *make) {
+    
+    make.edges.mas_equalTo(UIEdgeInsetsMake(20, 20, 20, 20));
+}];
+    
+}
 @end
