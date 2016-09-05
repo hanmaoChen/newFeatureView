131f33859cbd5e1f6ae937e65ebfdec3cb90f20d
hanmaoChen <771536653@qq.com>
2016-09-05 19:45:00 +0800

    Initial Commit

---
+++ newCharacteristic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newCharacteristic
+//
+//  Created by  chenhanmao on 9/5/16.
+//  Copyright © 2016  chenhanmao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ newCharacteristic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  newCharacteristic
+//
+//  Created by  chenhanmao on 9/5/16.
+//  Copyright © 2016  chenhanmao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
