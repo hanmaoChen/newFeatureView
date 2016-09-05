72955f4ea9360b4a28a6977b47ce786e59a328ce
hanmaoChen <771536653@qq.com>
2016-09-05 20:23:18 +0800

    项目准备及导入素材

--- ZFBMainViewController.h
+++ ZFBMainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ZFBMainViewController.h
 //  newCharacteristic
 //
 //  Created by  chenhanmao on 9/5/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ZFBMainViewController : UIViewController
 
 @end
-
---
+++ ZFBMainViewController.m
@@ -0,0 +1,13 @@
+//
+//  ZFBMainViewController.m
+//  newCharacteristic
+//
+//  Created by  chenhanmao on 9/5/16.
+//  Copyright © 2016  chenhanmao. All rights reserved.
+//
+
+#import "ZFBMainViewController.h"
+
+@implementation ZFBMainViewController
+
+@end
